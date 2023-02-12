6431813eb1607cd77f90324813ec18c4bf254619
Erik Martin <techgeek1129@gmail.com>
2023-02-12 10:41:38 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function init() {
+  var canvas = document.getElementById("webgl-canvas");
+  gl = canvas.getContext("webgl2");
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  cone = new Cone(gl, 40);
+  render();
+}
+
+function render() {
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	cone.render();
+}
+
+window.onload = init;
